3a69ac1f9afdfa00a3e1fc242c70d5a7b885fbee
jhg666 <112238672+jhg666@users.noreply.github.com>
2022-11-14 23:11:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-print('이 장기는 미니 장기입니다. 1은 쫄, 2는 차, 3은 왕이며 상대방의 왕을 잡으면 이기는 게임입니다.')
+print('파이썬으로 만들어진 장기입니다. 1은 쫄, 2는 차, 3은 상, 4는 마, 5는 포, 6은 사, 7은 왕이며 상대방의 왕을 잡으면 이기는 게임입니다.')
 print()
-print("쫄과 차, 왕의 이동 범위는 기존 장기의 룰과 같습니다.")
+print("기물의 움직임은 기존 장기의 법칙과 동일합니다.")
 print()
-print("왕은 시작 위치에서 주변 한 칸 씩까지만 움직일 수 있으며, 그 영역을 벗어날 수는 없습니다.")
+print("기물을 선택하실 때, 그리고 움직이실 때 세로의 좌표, 가로의 좌표 순으로 입력하시면 됩니다.")
+print()
+print("좌표는 0에서 시작하며, 아래로 내려갈수록 세로의 좌표의 숫자는 커지고, 오른쪽으로 갈수록 가로의 좌표의 숫자는 커집니다.")
 print()
 print('게임을 시작하겠습니다.')
 print()
 
-a = [[2, 0, 3, 0, 2],
-     [1, 0, 1, 0, 1],
-     [0, 0, 0, 0, 0],
-     [1, 0, 1, 0, 1],
-     [2, 0, 3, 0, 2]]
+a = [[2, 3, 4, 6, 0, 6, 4, 3, 2],
+     [0, 0, 0, 0, 7, 0, 0, 0, 0],
+     [0, 5, 0, 0, 0, 0, 0, 5, 0],
+     [1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [0, 5, 0, 0, 0, 0, 0, 5, 0],
+     [0, 0, 0, 0, 7, 0, 0, 0, 0],
+     [2, 3, 4, 6, 0, 6, 4, 3, 2]]
 
 while True:
 
@@ -23,7 +30,7 @@ while True:
 
     while True:
         row, column = map(int, input().split())
-        if 0 <= row < 5 and 0 <= column < 5:
+        if 0 <= row < 10 and 0 <= column < 9:
             if a[row][column] != 0:
                 b = a[row][column]
                 a[row][column] = 0
@@ -36,7 +43,7 @@ while True:
     if b == 1:
         while True:
             row, column = map(int, input().split())
-            if 0 <= row < 5 and 0 <= column < 5:
+            if 0 <= row < 10 and 0 <= column < 9:
                 if a[row][column] == 0:
                     a[row][column] = 1
                     break
@@ -47,6 +54,18 @@ while True:
                     a[row][column] = 1
                     break
                 if a[row][column] == 3:
+                    a[row][column] = 1
+                    break
+                if a[row][column] == 4:
+                    a[row][column] = 1
+                    break
+                if a[row][column] == 5:
+                    a[row][column] = 1
+                    break
+                if a[row][column] == 6:
+                    a[row][column] = 1
+                    break
+                if a[row][column] == 7:
                     a[row][column] = 1
                     break
             print("다시 입력해 주세요.")
@@ -54,7 +73,7 @@ while True:
     if b == 2:
         while True:
             row, column = map(int, input().split())
-            if 0 <= row < 5 and 0 <= column < 5:
+            if 0 <= row < 10 and 0 <= column < 9:
                 if a[row][column] == 0:
                     a[row][column] = 2
                     break
@@ -65,6 +84,18 @@ while True:
                     a[row][column] = 2
                     break
                 if a[row][column] == 3:
+                    a[row][column] = 2
+                    break
+                if a[row][column] == 4:
+                    a[row][column] = 2
+                    break
+                if a[row][column] == 5:
+                    a[row][column] = 2
+                    break
+                if a[row][column] == 6:
+                    a[row][column] = 2
+                    break
+                if a[row][column] == 7:
                     a[row][column] = 2
                     break
             print("다시 입력해 주세요.")
@@ -72,7 +103,7 @@ while True:
     if b == 3:
         while True:
             row, column = map(int, input().split())
-            if 0 <= row < 5 and 0 <= column < 5:
+            if 0 <= row < 10 and 0 <= column < 9:
                 if a[row][column] == 0:
                     a[row][column] = 3
                     break
@@ -85,9 +116,141 @@ while True:
                 if a[row][column] == 3:
                     a[row][column] = 3
                     break
+                if a[row][column] == 4:
+                    a[row][column] = 3
+                    break
+                if a[row][column] == 5:
+                    a[row][column] = 3
+                    break
+                if a[row][column] == 6:
+                    a[row][column] = 3
+                    break
+                if a[row][column] == 7:
+                    a[row][column] = 3
+                    break
             print("다시 입력해 주세요.")
 
-    if a[0][1] != 3 and a[0][2] != 3 and a[0][3] != 3 and a[1][1] != 3 and a[1][2] != 3 and a[1][3] != 3:
+    if b == 4:
+        while True:
+            row, column = map(int, input().split())
+            if 0 <= row < 10 and 0 <= column < 9:
+                if a[row][column] == 0:
+                    a[row][column] = 4
+                    break
+                if a[row][column] == 1:
+                    a[row][column] = 4
+                    break
+                if a[row][column] == 2:
+                    a[row][column] = 4
+                    break
+                if a[row][column] == 3:
+                    a[row][column] = 4
+                    break
+                if a[row][column] == 4:
+                    a[row][column] = 4
+                    break
+                if a[row][column] == 5:
+                    a[row][column] = 4
+                    break
+                if a[row][column] == 6:
+                    a[row][column] = 4
+                    break
+                if a[row][column] == 7:
+                    a[row][column] = 4
+                    break
+            print("다시 입력해 주세요.")
+
+    if b == 5:
+        while True:
+            row, column = map(int, input().split())
+            if 0 <= row < 10 and 0 <= column < 9:
+                if a[row][column] == 0:
+                    a[row][column] = 5
+                    break
+                if a[row][column] == 1:
+                    a[row][column] = 5
+                    break
+                if a[row][column] == 2:
+                    a[row][column] = 5
+                    break
+                if a[row][column] == 3:
+                    a[row][column] = 5
+                    break
+                if a[row][column] == 4:
+                    a[row][column] = 5
+                    break
+                if a[row][column] == 5:
+                    a[row][column] = 5
+                    break
+                if a[row][column] == 6:
+                    a[row][column] = 5
+                    break
+                if a[row][column] == 7:
+                    a[row][column] = 5
+                    break
+            print("다시 입력해 주세요.")
+
+    if b == 6:
+        while True:
+            row, column = map(int, input().split())
+            if 0 <= row < 10 and 0 <= column < 9:
+                if a[row][column] == 0:
+                    a[row][column] = 6
+                    break
+                if a[row][column] == 1:
+                    a[row][column] = 6
+                    break
+                if a[row][column] == 2:
+                    a[row][column] = 6
+                    break
+                if a[row][column] == 3:
+                    a[row][column] = 6
+                    break
+                if a[row][column] == 4:
+                    a[row][column] = 6
+                    break
+                if a[row][column] == 5:
+                    a[row][column] = 6
+                    break
+                if a[row][column] == 6:
+                    a[row][column] = 6
+                    break
+                if a[row][column] == 7:
+                    a[row][column] = 6
+                    break
+            print("다시 입력해 주세요.")
+
+    if b == 7:
+        while True:
+            row, column = map(int, input().split())
+            if 0 <= row < 10 and 0 <= column < 9:
+                if a[row][column] == 0:
+                    a[row][column] = 7
+                    break
+                if a[row][column] == 1:
+                    a[row][column] = 7
+                    break
+                if a[row][column] == 2:
+                    a[row][column] = 7
+                    break
+                if a[row][column] == 3:
+                    a[row][column] = 7
+                    break
+                if a[row][column] == 4:
+                    a[row][column] = 7
+                    break
+                if a[row][column] == 5:
+                    a[row][column] = 7
+                    break
+                if a[row][column] == 6:
+                    a[row][column] = 7
+                    break
+                if a[row][column] == 7:
+                    a[row][column] = 7
+                    break
+            print("다시 입력해 주세요.")
+
+    if a[0][3] != 7 and a[0][4] != 7 and a[0][5] != 7 and a[1][3] != 7 and a[1][4] != 7 and a[1][5] != 7 and a[2][3] != 7 and a[2][4] != 7 and a[2][5] != 7:
         print("게임이 끝났습니다.")
         print()
         print("승자는 플레이어 2입니다.")
@@ -95,10 +258,11 @@ while True:
         print("수고하셨습니다.")
         break
 
-    if a[3][1] != 3 and a[3][2] != 3 and a[3][3] != 3 and a[4][1] != 3 and a[4][2] != 3 and a[4][3] != 3:
+    if a[7][3] != 7 and a[7][4] != 7 and a[7][5] != 7 and a[8][3] != 7 and a[8][4] != 7 and a[8][5] != 7 and a[9][3] != 7 and a[9][4] != 7 and a[9][5] != 7:
         print("게임이 끝났습니다.")
         print()
         print("승자는 플레이어 1입니다.")
         print()
         print("수고하셨습니다.")
         break
+
